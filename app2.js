01534b3d48ec6398210de62f33bf30f06134baad
ahmed-razic <ahmed.razic@gmail.com>
2022-04-04 14:16:47 +0200

    finished

--- app2.js
+++ app2.js
@@ -1,4 +1,5 @@
 document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
 
 function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
@@ -22,6 +23,33 @@ function loadCustomer(e) {
       `;
 
       document.getElementById('customer').innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
+
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', './customers.json', true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+      let output;
+      customers.forEach((customer) => {
+        output += `
+          <ul>
+            <li>ID: ${customer.id}</li>
+            <li>Name: ${customer.name}</li>
+            <li>Company: ${customer.company}</li>
+            <li>Phone: ${customer.phone}</li>
+          </ul>
+      `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
     }
   };
 
