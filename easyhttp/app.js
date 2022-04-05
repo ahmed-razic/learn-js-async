2679bd8da37a738a50b6f4fdc6ab4e7e4cf4b4bf
ahmed-razic <ahmed.razic@gmail.com>
2022-04-05 14:00:30 +0200

    finished easyhttp

--- easyhttp/app.js
+++ easyhttp/app.js
@@ -7,3 +7,40 @@ http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
     console.log(posts);
   }
 });
+
+const data = { title: 'New Post', body: 'Body of new post' };
+
+http.post(
+  'https://jsonplaceholder.typicode.com/posts',
+  data,
+  function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+  }
+);
+
+http.put(
+  'https://jsonplaceholder.typicode.com/posts/10',
+  data,
+  function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+  }
+);
+
+http.delete(
+  'https://jsonplaceholder.typicode.com/posts/10',
+  function (err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
+    }
+  }
+);
--- easyhttp/easyhttp.js
+++ easyhttp/easyhttp.js
@@ -22,7 +22,46 @@ easyHTTP.prototype.get = function (url, callback) {
 };
 
 //Make an HTTP POST REQUEST
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 //Make an HTTP PUT REQUEST
 
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
 //Make an HTTP DELETE REQUEST
+
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this; //because this in function has its own scope; it is not necessary for the arrow functions
+
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error: ' + self.http.status, null);
+    }
+  };
+
+  this.http.send();
+};
