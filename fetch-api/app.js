495c5786be566919d8bec4ce2138a43c681aae0b
ahmed-razic <ahmed.razic@gmail.com>
2022-04-05 21:05:50 +0200

    fetch api finished

--- fetch-api/app.js
+++ fetch-api/app.js
@@ -1,5 +1,9 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJSON);
+
+document.getElementById('button3').addEventListener('click', getAPIData);
+
 function getText() {
   window
     .fetch('./text.txt')
@@ -8,6 +12,42 @@ function getText() {
     })
     .then(function (data) {
       document.getElementById('output').innerHTML = data;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getJSON() {
+  window
+    .fetch('./posts.json')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let output = '';
+      data.forEach(function (post) {
+        output += `<hr/><li>${post.title}</li> <li>${post.body}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getAPIData() {
+  window
+    .fetch('https://api.github.com/users')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let output = '';
+      data.forEach(function (user) {
+        output += `<hr/><li>${user.id}</li> <li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
     })
     .catch(function (err) {
       console.log(err);
